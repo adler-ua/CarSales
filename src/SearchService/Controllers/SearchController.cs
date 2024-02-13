954fb6d2f0053a606f45df83c89e6830b0d98261
adler-ua <adler.ua@gmail.com>
2024-02-13 16:24:02 +0200

    Search pagination

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -8,16 +8,24 @@ namespace SearchService;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber=1, int pageSize=4)
     {
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>();
         query.Sort(x => x.Ascending(i => i.Make));
 
         if(!string.IsNullOrEmpty(searchTerm))
         {
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
+
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         var result = await query.ExecuteAsync();
-        return result;
+        return Ok(new {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
