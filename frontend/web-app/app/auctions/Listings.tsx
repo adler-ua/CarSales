2ca79d5bdeeb65254a8ce8b796e525fe96d0663b
adler-ua <adler.ua@gmail.com>
2024-02-23 13:52:39 +0200

    Added Page Size filter

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -5,24 +5,28 @@ import AuctionCard from './AuctionCard';
 import { Auction } from '@/types';
 import AppPagination from '../components/AppPagination';
 import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
 
 export default /*async*/ function Listings() {
+  /* using local state: */
   const [auctions, setAuctions] = useState<Auction[]>([]);
   const [pageCount, setPageCount] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(4);
 
   useEffect(() => {
-    getData(pageNumber).then(data => { 
+    getData(pageNumber, pageSize).then(data => { 
       setAuctions(data.results);
       setPageCount(data.pageCount);
-      setPageNumber(pageNumber);
+      //setPageNumber(pageNumber); // called when an event pageChanged is triggered
     })
-  }, /* dependencies for the method to be called: */ [pageNumber]) /* whenever pageNumber changes -- useEffect gets called again */
+  }, /* dependencies for the method to be called: */ [pageNumber, pageSize]) /* whenever pageNumber changes -- useEffect gets called again */
 
   if(auctions.length === 0) return <h3>Loading...</h3>
 
   return (
     <>
+      <Filters pageSize={pageSize} setPageSize={setPageSize}/>
       <div className='grid grid-cols-4 gap-6'>
           {auctions.map(auction => (
               <AuctionCard auction={auction} key={auction.id} />
