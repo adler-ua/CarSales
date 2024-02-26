d8f3519fb79341908e63def05b5b9c906b95f63e
adler-ua <adler.ua@gmail.com>
2024-02-26 17:04:46 +0200

    Added search functionality

---
+++ frontend/web-app/app/nav/Logo.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useParamsStore } from '@/hooks/useParamsStore'
+import React from 'react'
+import { AiOutlineCar } from 'react-icons/ai'
+
+export default function Logo() {
+    const reset = useParamsStore(state => state.reset);
+    return (
+        <div className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'
+            onClick={reset}>
+            <AiOutlineCar size={34} />
+            <div>Car Sales</div>
+        </div>
+    )
+}
