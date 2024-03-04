10dad756954d532d1d9bbfb01b3942e7b137492f
adler-ua <adler.ua@gmail.com>
2024-03-04 14:04:32 +0200

    Added GRPC Server side code in AuctionService

---
+++ src/AuctionService/Services/GrpcAuctionService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+
+namespace AuctionService;
+
+public class GrpcAuctionService : GrpcAuction.GrpcAuctionBase
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public GrpcAuctionService(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override async Task<GrpcAuctionResponse> GetAuction(GetAuctionRequest request, ServerCallContext context)
+    {
+        Console.WriteLine("==> Received GRPC request GetAuction");
+
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(request.Id)) 
+            ?? throw new RpcException(new Status(StatusCode.NotFound,"Not Found"));
+        var response = new GrpcAuctionResponse(){
+            Auction = new GrpcAuctionModel(){
+                Id = auction.Id.ToString(),
+                AuctionEnd = auction.AuctionEnd.ToString(),
+                ReservePrice = auction.ReservePrice,
+                Seller = auction.Seller
+            }
+        };
+        return response;
+    }
+}
